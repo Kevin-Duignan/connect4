361048eed6bfd2529e3ae3d14016460b958f011f
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-03-13 10:16:36 +1100

    Create python file with template code.

---
+++ connect4.py
@@ -0,0 +1,186 @@
+"""
+FIT1045: Sem 1 2023 Assignment 1 (Solution Copy)
+"""
+import random
+import os
+
+def clear_screen():
+	"""
+	Clears the terminal for Windows and Linux/MacOS.
+
+	:return: None
+	"""
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def print_rules():
+	"""
+	Prints the rules of the game.
+
+	:return: None
+	"""
+	print("================= Rules =================")
+	print("Connect 4 is a two-player game where the")
+	print("objective is to get four of your pieces")
+	print("in a row either horizontally, vertically")
+	print("or diagonally. The game is played on a")
+	print("6x7 grid. The first player to get four")
+	print("pieces in a row wins the game. If the")
+	print("grid is filled and no player has won,")
+	print("the game is a draw.")
+	print("=========================================")
+
+
+def validate_input(prompt, valid_inputs):
+	"""
+	Repeatedly ask user for input until they enter an input
+	within a set valid of options.
+
+	:param prompt: The prompt to display to the user, string.
+	:param valid_inputs: The range of values to accept, list
+	:return: The user's input, string.
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def create_board():
+	"""
+	Returns a 2D list of 6 rows and 7 columns to represent
+	the game board. Default cell value is 0.
+
+	:return: A 2D list of 6x7 dimensions.
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def print_board(board):
+	"""
+	Prints the game board to the console.
+
+	:param board: The game board, 2D list of 6x7 dimensions.
+	:return: None
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def drop_piece(board, player, column):
+	"""
+	Drops a piece into the game board in the given column.
+	Please note that this function expects the column index
+	to start at 1.
+
+	:param board: The game board, 2D list of 6x7 dimensions.
+	:param player: The player who is dropping the piece, int.
+	:param column: The index of column to drop the piece into, int.
+	:return: True if piece was successfully dropped, False if not.
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def execute_player_turn(player, board):
+	"""
+	Prompts user for a legal move given the current game board
+	and executes the move.
+
+	:return: Column that the piece was dropped into, int.
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def end_of_game(board):
+	"""
+	Checks if the game has ended with a winner
+	or a draw.
+
+	:param board: The game board, 2D list of 6 rows x 7 columns.
+	:return: 0 if game is not over, 1 if player 1 wins, 2 if player 2 wins, 3 if draw.
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def local_2_player_game():
+	"""
+	Runs a local 2 player game of Connect 4.
+
+	:return: None
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def main():
+	"""
+	Defines the main application loop.
+    User chooses a type of game to play or to exit.
+
+	:return: None
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def cpu_player_easy(board, player):
+	"""
+	Executes a move for the CPU on easy difficulty. This function 
+	plays a randomly selected column.
+
+	:param board: The game board, 2D list of 6x7 dimensions.
+	:param player: The player whose turn it is, integer value of 1 or 2.
+	:return: Column that the piece was dropped into, int.
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def cpu_player_medium(board, player):
+	"""
+	Executes a move for the CPU on medium difficulty. 
+	It first checks for an immediate win and plays that move if possible. 
+	If no immediate win is possible, it checks for an immediate win 
+	for the opponent and blocks that move. If neither of these are 
+	possible, it plays a random move.
+
+	:param board: The game board, 2D list of 6x7 dimensions.
+	:param player: The player whose turn it is, integer value of 1 or 2.
+	:return: Column that the piece was dropped into, int.
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def cpu_player_hard(board, player):
+	"""
+	Executes a move for the CPU on hard difficulty.
+	This function creates a copy of the board to simulate moves.
+	<Insert player strategy here>
+
+	:param board: The game board, 2D list of 6x7 dimensions.
+	:param player: The player whose turn it is, integer value of 1 or 2.
+	:return: None
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+def game_against_cpu():
+	"""
+	Runs a game of Connect 4 against the computer.
+
+	:return: None
+	"""
+	# Implement your solution below
+	raise NotImplementedError
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+
