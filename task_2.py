9748efb44649116c37c7d2b344b9ad45040eaf4e
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-03-14 09:45:00 +1100

    Implement task_2.py file

--- task_2.py
+++ task_2.py
@@ -1,15 +1,20 @@
 # Kevin
 def create_board():
-	"""
-	Returns a 2D list of 6 rows and 7 columns to represent
-	the game board. Default cell value is 0.
+    """
+    Returns a 2D list of 6 rows and 7 columns to represent
+    the game board. Default cell value is 0.
 
-	:return: A 2D list of 6x7 dimensions.
-	"""
-	# Implement your solution below
-	raise NotImplementedError
+    :return: A 2D list of 6x7 dimensions.
+    """
+    # Implement your solution below
+    return [[0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]]
 
 
 if __name__ == "__main__":
-	# Enter test code below
-	print(create_board())
+    # Enter test code below
+    print(create_board())
