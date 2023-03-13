715d602439dbbce7db1b6933694ad14dfc473b67
jlouey01 <127797723+jlouey01@users.noreply.github.com>
2023-03-13 22:50:31 +0000

    task 1 edited

--- task_1.py
+++ task_1.py
@@ -1,25 +1,24 @@
-Oliver
+# Oliver
 def validate_input(prompt, valid_inputs):
-	"""
-	Repeatedly ask user for input until they enter an input
-	within a set valid of options.
+    """
+    Repeatedly ask user for input until they enter an input
+    within a set valid of options.
 
-	:param prompt: The prompt to display to the user, string.
-	:param valid_inputs: The range of values to accept, list
-	:return: The user's input, string.
-	"""
-	# Implement your solution below
-print(prompt,end="")
-enter = input()
-if enter in valid_inputs:
-    return enter
-else:
-    print("Invalid input, please try again.")
-    return validate_input(prompt,valid_inputs)
-    
-raise NotImplementedError
+    :param prompt: The prompt to display to the user, string.
+    :param valid_inputs: The range of values to accept, list
+    :return: The user's input, string.
+    """
+    # Implement your solution below
+    print(prompt, end="")
+    enter = input()
+    if enter in valid_inputs:
+        return enter
+    else:
+        print("Invalid input, please try again.")
+        return validate_input(prompt, valid_inputs)
 
 
 if __name__ == "__main__":
-	# Enter test code below
-	user_input = validate_input("Please select an option (a, b, c): ", ["a", "b", "c"])
+    # Enter test code below
+    user_input = validate_input(
+        "Please select an option (a, b, c): ", ["a", "b", "c"])
