6cbc7905c037cc85f2af3ce49804685dcebfa32a
jlouey01 <127797723+jlouey01@users.noreply.github.com>
2023-03-14 03:46:31 +0000

    task 3 almost done

--- task_3.py
+++ task_3.py
@@ -6,15 +6,38 @@ def print_board(board):
 	:return: None
 	"""
 	# Implement your solution below
-	raise NotImplementedError
+	board_row = 6
+	board_col = 7
+	print("========== Connect4 =========")
+	print("Player 1: X       Player 2: O\n")
+	print("  1   2   3   4   5   6   7")
+	print(" --- --- --- --- --- --- ---")
+	for row in range(board_row):
+		print("| ", end = "")
+		for col in range(board_col):
+			if board[row][col] == 1:
+				print("X |", end = " ")
+			elif board[row][col] == 2:
+				print("O |", end = " ")
+			elif board[row][col] == 0:
+				print("  |", end = " ")
+		print("\n --- --- --- --- --- --- ---")
+	print("=============================")
 
+		
+	
 
 def create_board():
-	# Copy your solution from task 2 here
-	raise NotImplementedError
+
+    # Implement your solution below
+    board_row = 6
+    board_col = 7
+    board = [[0 for i in range(board_col)] for i in range(board_row)]
+    return board
 
 
 if __name__ == "__main__":
 	# Enter test code below
 	board = create_board()
+	board[5][0], board[5][1] = 1, 2 # place a 1 in bottom left cell and a 2 in the cell directly to the right
 	print_board(board)
