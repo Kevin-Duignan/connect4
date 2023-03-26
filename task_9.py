087862a24a783441f713e530f2d0640c5f7ab5b7
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-03-26 17:28:51 +1100

    Add task_9.py

--- task_9.py
+++ task_9.py
@@ -1,6 +1,28 @@
+# Kevin
 import random
 
 # Copy and paste drop_piece here
+def drop_piece(board, player, column):
+	"""
+	Drops a piece into the game board in the given column.
+	Please note that this function expects the column index
+	to start at 1.
+
+	:param board: The game board, 2D list of 6x7 dimensions.
+	:param player: The player dropping the piece, int.
+	:param column: The index of column to drop the piece into, int.
+	:return: True if piece was successfully dropped, False if not.
+	"""
+	# Iterate through each row from bottom up
+	if 1 <= column <= 7:
+		for row in reversed(board):
+			# Drop player token in the lowest free space
+			if row[column - 1] == 0:
+				row[column - 1] = player
+				return True
+
+	# If all spaces are filled (no 0s)
+	return False
 
 def cpu_player_easy(board, player):
 	"""
@@ -12,4 +34,16 @@ def cpu_player_easy(board, player):
 	:return: Column that the piece was dropped into, int.
 	"""
 	# Implement your solution below
-	raise NotImplementedError
+	random_column = random.randint(1, 7)
+	while True:
+		if drop_piece(board, player, random_column):
+			return random_column
+
+print(cpu_player_easy([
+[1, 1, 1, 1, 1, 0, 1],
+[1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1],
+[1, 2, 2, 2, 1, 1, 1]
+], 2))
