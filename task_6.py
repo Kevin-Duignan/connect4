430b925979fe9f723df4c548f88d3771d2c65dfd
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-03-28 17:49:01 +1100

    Task 6 row and column check + return 0 or 3 implemented

--- task_6.py
+++ task_6.py
@@ -7,32 +7,43 @@ def end_of_game(board):  # Question 6 - Rohit
     :param board: The game board, 2D list of 6 rows x 7 columns.
     :return: 0 if game is not over, 1 if player 1 wins, 2 if player 2 wins, 3 if draw.
     """
-    # Containing strings of each row in original board
-    row_board = []
-    # Containing strings of each column in a row
-    column_board = []
+      
+    # # * Rows -> List of Strings
+    # for row in board:
+    #     row_str = "".join([str(i) for i in row])
+        
+    #     # Check if row has a win
+    #     if "1111" in row_str:
+    #         return 1
+    #     elif "2222" in row_str:
+    #         return 2
+            
+    # # * Columns -> List of Strings
+    # for i in range(len(board[0])): # Length of each row in original board
+    #     column_string = ""
+    #     # Concatenate elements of ith index from each row into a string
+    #     for row in board:
+    #         column_string += str(row[i])
+        
+    #     # Check if column has a win
+    #     if "1111" in column_string:
+    #         return 1
+    #     elif "2222" in column_string:
+    #         return 2
+        
     
-    # * Rows -> List of Strings
+
+    # * Keep Playing or Draw
     for row in board:
-        row_board.append("".join([str(i) for i in row])) # Can't append elements of instant int
-            
-    # * Columns -> List of Strings
-    for i in range(len(board[0])): # Length of each row in original board
-        column_string = ""
-        # Concatenate elements of ith index from each row into a string
-        for row in board:
-            column_string+= str(row[i])
-        column_board.append(column_string)
-            
-    print(row_board)
-    print(column_board)
-            
-            
+        if 0 in row:
+            return 0 # Still empty slots left
+    else:
+        return 3 # All slots are filled and no win     
     
 
 
 board = [
-    [1, 2, 1, 1, 2, 2, 0],
+    [1, 2, 1, 1, 2, 2, 1],
     [2, 1, 2, 2, 1, 2, 2],
     [2, 1, 1, 2, 1, 2, 1],
     [1, 1, 1, 1, 1, 1, 1],
